9e9245ada8749fe60e56d455f087c4b74ba76300
Dennis Nygren <dennis@pxlbros.com>
2015-06-01 11:44:46 -0700

    v1.0.48

--- dist/pxlcore.js
+++ dist/pxlcore.js
@@ -1663,7 +1663,7 @@ function pxlCore(options)
 
 pxlCore.prototype =
 {
-	version: '1.0.48',
+	version: '1.0.49',
 
 	options:
 	{
--- src/js/pxlcore.js
+++ src/js/pxlcore.js
@@ -5,7 +5,7 @@ function pxlCore(options)
 
 pxlCore.prototype =
 {
-	version: '1.0.48',
+	version: '1.0.49',
 
 	options:
 	{
